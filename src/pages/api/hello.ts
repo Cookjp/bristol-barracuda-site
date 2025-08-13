68f5119f50ce9b7aa25fa1d36dccb68d045fa22e
Cookjp <james.cook@motionmetrics.co>
2025-08-13 22:06:28 +0100

    log in hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,5 @@
 export const GET = async ({ request }) => {
+  console.log('Request received:', request);
   return new Response('Hello from Vercel!', {
     headers: { 'Content-Type': 'text/plain' }
   });
