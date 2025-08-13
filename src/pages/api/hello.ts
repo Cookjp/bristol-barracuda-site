d5b7f68380b6d246d2dcd67e423ad8cecea931ba
Cookjp <james.cook@motionmetrics.co>
2025-08-13 22:15:17 +0100

    add node mailer

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,29 @@
-export const GET = async ({ request }) => {
-  console.log('Request received:', request);
-  return new Response('Hello from Vercel!', {
-    headers: { 'Content-Type': 'text/plain' }
+import nodemailer from 'nodemailer';
+
+export const POST = async ({ request }) => {
+  const data = await request.formData();
+  const name = data.get('name');
+  const email = data.get('email');
+  const message = data.get('message');
+
+  // Configure your transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jamescook214@gmail.com',
+      pass: import.meta.env.MAIL_APP_PASSWORD,
+    },
   });
+
+  try {
+    await transporter.sendMail({
+      from: email?.toString(),
+      to: 'jaemscook214@gmail.com',
+      subject: `Contact Form Submission from ${name}`,
+      text: message?.toString(),
+    });
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
+  }
 };
